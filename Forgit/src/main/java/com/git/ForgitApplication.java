27b1f7c6bd8a0c21a1556a3e49af89489e6080a4
hareesha <harisha21.p@gmail.com>
2020-11-08 22:37:45 -0800

    first commit

---
+++ Forgit/src/main/java/com/git/ForgitApplication.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgitApplication.class, args);
+	}
+
+}
---
+++ Forgit/src/main/java/com/git/WebController.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+@GetMapping("/hello")
+public String sayHello()
+{
+	return "hello";
+}
+
+}
